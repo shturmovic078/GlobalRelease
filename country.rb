f065cc236c7ac641e70c11cbc31c0aa07af77729
shturmovic078 <agolubev1@usnycpww4255.global.publicisgroupe.net>
2016-07-20 09:56:31 -0400

    Country selector updated
    pulls 12 country selector for each brand

---
+++ country.rb
@@ -0,0 +1,51 @@
+#Albert Golubev
+
+
+require 'watir-webdriver'
+require 'webdriver-user-agent'
+require 'csv'
+require 'watir-scroll'
+#require 'mechanize'
+
+include Selenium
+
+caps = WebDriver::Remote::Capabilities.new
+caps[:os] = "Windows"
+caps[:name] = "Watir WebDriver"
+caps[:browser] = 'IE'
+caps[:browser_version] = '11.0'
+caps['os'] = 'Windows'
+caps['os_version'] = '7'
+caps['resolution'] = '1024x768'
+caps["browserstack.debug"] = "true"
+
+browser = Watir::Browser.new(:remote,
+  :url => "http://zaqwsx1:Fs54nwmULt7BaSTosZxi@hub-cloud.browserstack.com/wd/hub",
+  :desired_capabilities => caps)
+counter = 0
+
+csv_text = File.read('country.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|  
+sleep 4
+loc1= "#{row['url']}"
+#loc2= "#{row['product']}"
+#loc3= "#{row['type']}"
+#loc4= "#{row['count']}"
+sleep 1
+counter+=1
+browser.goto loc1
+puts loc1
+sleep 1
+puts counter
+puts browser.title
+
+sleep 2
+browser.element(:css => "a.language").click
+sleep 3
+
+browser.screenshot.save  "Country/#{"Blank_Country"}_"<<counter.to_s<<".png"
+
+
+
+end
